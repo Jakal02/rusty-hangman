7dfde1e574d10b6cacb34e17bd739181824bdf67
Jakal02 <gitcontribs@eligrubbs.com>
2024-07-07 17:37:41 -0400

    pipe len arg to Game struct. Created dummy function to select secret word.

--- src/game.rs
+++ src/game.rs
@@ -6,13 +6,17 @@ pub struct Game {
 
 
 impl Game {
-    pub fn new() -> Game {
+    pub fn new(s_len: u8) -> Game {
         Game {
-            secret_word: "secret".to_string(),
+            secret_word: Self::select_secret_word(s_len),
         }
     }
 
     pub fn get_secret(&self) -> &String {
         &self.secret_word
+    }
+
+    fn select_secret_word(_s_len: u8) -> String {
+        "secret".to_string()
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod game;
 use game::Game;
 
 pub fn run(secret_len: u8) {
-    let game = Game::new();
+    let game = Game::new(secret_len);
 
     println!("The secret word is {} letters long.", secret_len);
     println!("Hello Hangman. {}", game.get_secret())
