22821b4a4b6d751b7e2636320f90155abda01b63
Jakal02 <gitcontribs@eligrubbs.com>
2024-07-07 23:12:41 -0400

    whole game except working hangman graphic.

--- src/game.rs
+++ src/game.rs
@@ -1,21 +1,24 @@
 use std::fmt;
-
+use std::collections::HashSet;
+use std::io::{stdin, stdout, Read, Write};
 
 pub struct Game {
     secret_word: String,
-    previous_guesses: Vec<char>,
-    is_over: bool,
+    previous_guesses: HashSet<char>,
     num_guesses: u8,
 }
 
+pub enum GameResult {
+    Stumped,
+    Guessed,
+}
 
 impl Game {
     pub fn new(s_len: u8) -> Game {
         Game {
             secret_word: Self::select_secret_word(s_len),
-            previous_guesses: vec![],
-            is_over: false,
-            num_guesses: 8,
+            previous_guesses: vec![].into_iter().collect(),
+            num_guesses: 6,
         }
     }
 
@@ -24,22 +27,21 @@ impl Game {
     }
 
     fn select_secret_word(_s_len: u8) -> String {
-        "secret".to_string()
+        "secret".to_string().to_uppercase()
     }
 
     
     fn first_char_from_str(&self, s: &String) -> char {
-        s.as_str().chars().next().unwrap()
+        s.as_str().chars().next().unwrap().to_uppercase().next().unwrap()
     }
 
     /// Returns uppercase 1-character guess if user inputs one upper
     /// or lower case ASCII character (A-Z and a-z).  
     /// Returns GuessError if the input is NOT 1 character excluding
     /// newline or is NOT an ASCII character.
-    pub fn get_user_guess(&self) -> Result<char, GuessError> {
-        use std::io::{stdin,stdout,Write};
+    pub fn one_char_user_input(&mut self) -> Result<char, GuessError> {
+
         let mut s: String = String::new();
-        print!("Guess a character: ");
         let _ = stdout().flush();
         stdin().read_line(&mut s).expect("Did not enter a correct string");
 
@@ -51,16 +53,103 @@ impl Game {
         if let Some('\r')=s.chars().next_back() {
             s.pop();
         }
-    
-        if s.len() != 1 || !self.first_char_from_str(&s).is_ascii_alphabetic() {
+        let first: char = self.first_char_from_str(&s);
+
+        if s.len() != 1 || !first.is_ascii_alphabetic() || self.previous_guesses.contains(&first){
             return Err(GuessError{input: s});
         }
     
-        let first: char = self.first_char_from_str(&s);
-    
-        Ok(first.to_uppercase().next().unwrap())
+        Ok(first)
     }
 
+    /// repeatedly prompts user to enter valid char.  
+    /// Records the guess internally, and returns it for convenience
+    pub fn let_user_guess(&mut self) -> char{
+        let guess: char = loop {
+            print!("Enter your next guess: ");
+            match self.one_char_user_input() {
+                Ok(c) => break c,
+                Err(err) => println!("{}", err),
+            }
+        };
+        self.record_guess(guess);
+        guess
+    }
+
+    /// Record this guess and decrease number of guesses left 
+    /// if `guess` is not in the secret word.
+    fn record_guess(&mut self, guess:char) {
+        self.previous_guesses.insert(guess);
+        self.previous_guesses.insert(guess);
+        if !self.secret_word.contains(guess) {
+            self.num_guesses -= 1;
+        }
+        
+    }
+
+    pub fn is_over(&self) -> Option<GameResult> {
+        if self.num_guesses == 0 {
+            return Some(GameResult::Stumped)
+        }
+        let secret_chars: HashSet<char> = self.secret_word.chars().into_iter().collect();
+        if secret_chars.is_subset(&self.previous_guesses) {
+            return Some(GameResult::Guessed)
+        }
+        None
+    }
+
+    pub fn start_screen(&self) {
+        println!(r" 
+==================================================
+                  Welcome To...                   
+                                                  
+  _   _   ___   _   _ _____ ___  ___  ___   _   _ 
+ | | | | / _ \ | \ | |  __ \|  \/  | / _ \ | \ | |
+ | |_| |/ /_\ \|  \| | |  \/| .  . |/ /_\ \|  \| |
+ |  _  ||  _  || . ` | | __ | |\/| ||  _  || . ` |
+ | | | || | | || |\  | |_\ \| |  | || | | || |\  |
+ \_| |_/\_| |_/\_| \_/\____/\_|  |_/\_| |_/\_| \_/
+                                                  
+
+          (Press enter key to continue)            ");
+    let _ = stdout().flush();
+    stdin().read(&mut [0]).unwrap();
+    }
+
+    /// prints current game information
+    pub fn print_game_screen(&self) {
+        println!(r" 
+==================================================
+                {}                   
+                                                  
+                    +---+
+                    |   |
+                    |   O
+                    |  /|\
+                    |  / 
+                    |
+                =========                                              
+    {}", self.get_secret_word_progress(), self.get_string_of_previous_guesses());
+    }
+
+    /// Get string representing progress on the guess of the secret word
+    /// 
+    fn get_secret_word_progress(&self) -> String {
+
+        let string: String = self.secret_word.chars().into_iter()
+            .map(|x| {
+                if self.previous_guesses.contains(&x) {
+                    format!("{} ", x.to_uppercase().next().unwrap())
+                } else {
+                    "_ ".to_string()
+                }
+            } ).collect();
+        string
+    }
+
+    fn get_string_of_previous_guesses(&self) -> String {
+        self.previous_guesses.iter().map(|x| format!("{} ", x.to_uppercase().next().unwrap())).collect()
+    }
 }
 
 
@@ -71,7 +160,7 @@ pub struct GuessError{
 
 impl fmt::Display for GuessError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} is not a valid character input.", self.input)
+        write!(f, "{} is not a valid character input or has already been guessed.", self.input)
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,28 @@
 
 pub mod game;
-use game::Game;
+use game::{Game, GameResult};
 
 pub fn run(secret_len: u8) {
-    let game: Game = Game::new(secret_len);
+    let mut game: Game = Game::new(secret_len);
 
-    println!("The secret word is {} letters long.", secret_len);
-    println!("Hello Hangman. {}", game.get_secret());
+    game.start_screen();
 
-    let guess: char = loop {
-        match game.get_user_guess() {
-            Ok(c) => break c,
-            Err(err) => println!("{}", err),
+    let result: GameResult = loop {
+        match game.is_over() {
+            Some(x) => break x,
+            None => {
+                game.print_game_screen();
+                game.let_user_guess();
+            }
         }
     };
 
-    println!("captured guess {}", guess);
+    match result {
+        GameResult::Guessed => {
+            println!("You guessed the secret word: {}", game.get_secret())
+        }
+        GameResult::Stumped => {
+            println!("You failed to guess {}... Better luck next time.", game.get_secret())
+        }
+    }
 }
